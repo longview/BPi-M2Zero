cbd81f3ef9ed6ba4cce02ba46a34e7579613816b
longview <robin@longview.be>
2022-07-26 12:55:24 +0200

    Update cs4272.py

--- cs4272.py
+++ cs4272.py
@@ -5,11 +5,6 @@ spi.open(0,0)
 spi.max_speed_hz = 50000
 spi.mode = 0
 
-# this script configures a CS4272 for I2S master mode at 48 kHz using a 12.288 MHz MCLK
-# note that the ATAPI modes may not be entirely correct, see the datasheet.
-
-# change this to whatever is the RESET_L line for the codec
-# or remove, it *should* work without resetting for SPI mode
 os.system("gpio mode 5 output")
 os.system("gpio write 5 0")
 time.sleep(0.1)
@@ -21,7 +16,12 @@ to_send = [0x20, 0x07, 0x03]
 retval = spi.xfer3(to_send)
 print(retval)
           # start, auto increment zero,
-to_send = [0x20, 0x81, 0b10101001, 0b1000110, 0b00111001, 0x0, 0x0, 0b00010000]
+to_send = [0x20, 0x81, 0b10101001, 0b1000110, 0b00111001, 26, 21, 0b00010000]
+retval = spi.xfer3(to_send)
+print(retval)
+
+# implement channel swap by default to fix up the swapping done in hardware
+to_send = [0x20, 0x03, 0b00110110]
 retval = spi.xfer3(to_send)
 print(retval)
 # following config, clear PDN
